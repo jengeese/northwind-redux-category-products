b4d9004d86a3954d9aa8db6bbadc719de63702cc
jengeese <cengizvardar10@hotmail.com>
2022-04-20 20:50:19 +0300

    react_backend

--- src/components/cart/CartDetail.js
+++ src/components/cart/CartDetail.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as cartActions from "../../redux/actions/cartActions";
-import { Badge, Table, Button } from "reactstrap";
+import { Table, Button } from "reactstrap";
 import alertify from "alertifyjs"; 
 
 
