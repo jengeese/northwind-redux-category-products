415de12db292af6bd4af40b93a1f2bf016ea0e04
jengeese <cengizvardar10@hotmail.com>
2022-04-20 07:44:52 +0300

    /cart_route

--- src/components/cart/CartSummary.js
+++ src/components/cart/CartSummary.js
@@ -3,8 +3,15 @@ import { NavDropdown, Nav, Badge } from "react-bootstrap";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as cartActions from "../../redux/actions/cartActions";
+import { Link } from "react-router-dom";
+import alertify from "alertifyjs"; 
 
 class CartSummary extends Component {
+  removeFromCart(product) {
+    this.props.actions.removeFromCart(product);
+    alertify.error(product.productName + " removed from cart");
+  }
+
   renderEmpty() {
     return <Nav.Link>Empty Cart</Nav.Link>;
   }
@@ -13,16 +20,24 @@ class CartSummary extends Component {
       <NavDropdown title="Your cart" id="basic-nav-dropdown">
         {this.props.cart.map((cartItem) => (
           <NavDropdown.Item key={cartItem.product.id}>
-            
             {cartItem.product.productName}{" "}
             <Badge bg="success">{cartItem.quantity}</Badge>{" "}
-            <Badge bg="danger" onClick={()=>this.props.actions.removeFromCart(cartItem.product)}>X</Badge>
+            <Badge
+              bg="danger"
+              onClick={() =>
+                this.removeFromCart(cartItem.product)
+              }
+            >
+              X
+            </Badge>
           </NavDropdown.Item>
         ))}
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <Link to={"/"}>Main page</Link>
+        <NavDropdown.Divider />
+        <Link to={"/cart"}>Go to cart</Link>
       </NavDropdown>
     );
   }
--- src/components/root/App.js
+++ src/components/root/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Navi from "../navi/navi"
 import {Container} from "reactstrap"
 import Dashboard from './Dashboard';
+import {Route, Routes} from "react-router-dom";
+import CartDetail from '../cart/CartDetail';
 
 function App() {
   return (
     <Container>
 <Navi></Navi>
-<Dashboard></Dashboard>
+<Routes>
+  <Route path="/" element={<Dashboard/>} />
+  <Route path="/product" element={<Dashboard/>}/>
+  <Route path="/cart" element={<CartDetail/>}/>
+</Routes>
     </Container>
   );
 }
